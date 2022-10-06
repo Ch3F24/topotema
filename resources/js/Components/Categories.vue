96221eac7017bd59508e2578620eb57569efd9e2
Chef <zsolt.horvath@wst.hu>
2022-10-06 17:08:57 +0200

    toggle all btn, css

--- resources/js/Components/Categories.vue
+++ resources/js/Components/Categories.vue
@@ -1,15 +1,18 @@
 <template>
     <span class="divider magnificent" @click="listShow = !listShow"></span>
-    <ul class="categories h-0 lg:h-full overflow-hidden transition-all" :class="{'h-full' : listShow}" ref="list">
+    <ul class="categories h-0 lg:h-full overflow-hidden lg:overflow-visible transition-all" :class="{'h-full' : listShow}" ref="list">
+        <li>
+            <p class="relative select-all" @click="toggleAll()" title="Mind"></p>
+        </li>
         <li>
             <label class="block">
-                <input type="checkbox" name="cat-1" value="cat-1"/>
+                <input type="checkbox" name="cat-1" v-model="filters.cat1"/>
                 <span>Az államvédelmi negyed</span>
             </label>
         </li>
         <li>
             <label class="block">
-                <input type="checkbox" name="cat-2" value="cat-2"/>
+                <input type="checkbox" name="cat-2" v-model="filters.cat2"/>
                 <span>Az államvédelmi negyed</span>
             </label>
         </li>
@@ -21,11 +24,22 @@ export default {
     name: "Categories",
     data() {
         return {
-            listShow: false
+            listShow: false,
+            filters: {
+                cat1 : false,
+                cat2 : false,
+                cat3 : false
+            }
         }
 
     },
     methods: {
+        toggleAll() {
+            Object.keys(this.filters).forEach((key) => {
+                this.filters[key] = true
+
+            })
+        }
     }
 
 }
